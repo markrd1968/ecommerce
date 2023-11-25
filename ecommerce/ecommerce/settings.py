397f0c862643413d78b645da5483d2ff2397f1df
Mark Russell Dyson <149165971+markrd1968@users.noreply.github.com>
2023-11-25 12:39:22 +0000

    Session set up
    session set up

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.views.categories',
+                'cart.context_processors.cart', 
+                
                 
             ],
         },
